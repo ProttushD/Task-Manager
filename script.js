40bb8df8b78fcab58c5707a191fc4e032e9b7198
Lucifer <143842404+ProttushD@users.noreply.github.com>
2026-06-18 15:11:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
 const inp = document.querySelector("input");
 const select = document.querySelector("select");
-const btn = document.querySelector("#task"); 
+const btn = document.querySelector("#task");
+const taskB = document.querySelector(".task-apear");
 
-const taskB = document.querySelector(".task-apear")
-
-
-btn.addEventListener('click' , () => {
- const value = inp.value;
- const choice = select.value;
+// above we have selected all the elements we are going to use till now 
 
 
- if(value.trim() === ""){
-    alert("Please Add the Task first")
-    return;
- }
-
- taskB.innerHTML += `
-
- <div class="task-box">
-                <div class="text">
-                    <h5>${value}</h5>
-                </div>
-                <div class="btns">
-                    <div class="type">${choice}</div>
-                    <button class="btn btn-1">Edit</button>
-                    <button class="btn btn-2">Complete</button>
-                    <button class="btn btn-3">Delete</button>
-                </div>
-            </div>
- `
 
 
- inp.value="";
-}) 
+// taskBox.textContent = inp.value;
 
+btn.addEventListener("click", () => {
+    // console.log(inp.value);
+    // console.log(select.value);
+
+
+    const taskBox = document.createElement("div"); // with this we are creating the html structure div
+    taskBox.className = "task-box"; // with this we are asigning the class name to div 
+
+    const textDiv = document.createElement("div");
+    textDiv.className = "text";
+
+    const btns = document.createElement("div");
+    btns.className = "btns";
+
+    const heading = document.createElement("h5");
+    heading.textContent = inp.value;
+
+    const category = document.createElement("div");
+    category.className = "type"
+    category.textContent = select.value
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "btn btn-1";
+    editBtn.textContent = "Edit";
+
+    const completeBtn = document.createElement("button");
+    completeBtn.className = "btn btn-2";
+    completeBtn.textContent = "Complete";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "btn btn-3";
+    deleteBtn.textContent = "Delete";
+
+    textDiv.append(heading);
+    btns.append(category);
+    btns.append(editBtn);
+    btns.append(completeBtn);
+    btns.append(deleteBtn);
+    taskBox.append(textDiv);
+    taskBox.append(btns);
+    taskB.append(taskBox);
+    
+   
+
+
+
+
+
+
+});
+
+// here the button is created though which we are going to add task 
