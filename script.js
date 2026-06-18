481840769804b65dbb1aafa098ab6f2245f5a827
Lucifer <143842404+ProttushD@users.noreply.github.com>
2026-06-18 10:54:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const inp = document.querySelector("input");
+const select = document.querySelector("select");
+const btn = document.querySelector("#task"); 
+
+const taskB = document.querySelector(".task-apear")
+
+
+btn.addEventListener('click' , () => {
+ const value = inp.value;
+ const choice = select.value;
+
+ taskB.innerHTML += `
+
+ <div class="task-box">
+                <div class="text">
+                    <h5>${value}</h5>
+                </div>
+                <div class="btns">
+                    <div class="type">${choice}</div>
+                    <button class="btn btn-1">Edit</button>
+                    <button class="btn btn-2">Complete</button>
+                    <button class="btn btn-3">Delete</button>
+                </div>
+            </div>
+ `
+
+
+
+//  console.log(value,choice)
+}) 
+
